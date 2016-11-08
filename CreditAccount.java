afa381f2043a45044b446046729cb90939440b03
urshmanning <urshmanning@gmail.com>
2016-11-08 11:08:51 +0000

    Update CreditAccount.java

--- CreditAccount.java
+++ CreditAccount.java
@@ -2,5 +2,48 @@ package SimpleBank;
 
 public class CreditAccount extends Account
 {
-
+  private double ODLimit;
+  
+  public CreditAccount()
+  {
+    this.Account();
+    ODLimit = 100;
+  }
+  
+  public CreditAccount(double l, Account a)
+  {
+    this.Account(a);
+    ODLimit = l;
+  }
+  
+  
+  public void setLimit(double l)
+  {
+    ODLimit = l;
+  }
+  
+  public double getLimit()
+  {
+    return ODLimit;
+  }
+  
+  public String ToString()
+  {
+    String acc = this.ToString()+"\nOD Limit"
+  }
+  
+  public void debit(double d)
+  {
+    if(d>(this.getBalance()+ODLimit))
+    {
+      throw(Exception e)
+      {
+        System.out.println("Insufficient Funds - Transaction Cancelled");
+      }
+    }
+    else
+    {
+      this.debit(amount);
+    }
+  }
 }
